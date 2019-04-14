c1723d4c68024cba41fbc738763b0797521eb288
Fabio <fabio.riganti01@universitadipavia.it>
2019-04-14 02:53:26 +0200

    Piccola correzione sulla posizione di image.jpg

--- src/Test.java
+++ src/Test.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 public class Test {
     public static void main(String[] args) {
         try {
-            ReadImage read = new ReadImage("C:\\Users\\fabio\\IdeaProjects\\ProvaThread\\src\\image.jpg");
+            ReadImage read = new ReadImage("src/image.jpg");
             System.out.println("Il valore di blu massimo trovato è "+read.getMaxblue()+".");
         } catch (IOException e) {
             System.err.println("Errore lettura immagine.");
